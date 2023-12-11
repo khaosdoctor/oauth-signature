dd9d85ea373d5067194c9558aecdda56cb9184af
Lucas Santos <lhs.santoss@gmail.com>
2023-12-11 23:52:38 +0100

    Update dependency version

--- src/deps.ts
+++ src/deps.ts
@@ -1,5 +1,5 @@
-export { encode as base64Encode } from 'https://deno.land/std@0.201.0/encoding/base64.ts'
-export { encode as hexEncode } from 'https://deno.land/std@0.201.0/encoding/hex.ts'
+export { encode as base64Encode } from 'https://deno.land/std@0.208.0/encoding/base64.ts'
+export { encode as hexEncode } from 'https://deno.land/std@0.208.0/encoding/hex.ts'
 export * from 'https://deno.land/std@0.208.0/crypto/crypto.ts'
 export { encode as percentEncode } from 'https://deno.land/x/percentencode@v1.0.2/mod.ts'
 export * from 'https://deno.land/std@0.208.0/assert/mod.ts'
